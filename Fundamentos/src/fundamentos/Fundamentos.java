f7cb0ab5b04482054fdeceb827a67ba2f89a156f
Victor Ferreira <victorluizferreira48@gmail.com>
2023-07-28 10:58:08 -0300

    Declarando String como Objeto

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -1,12 +1,13 @@
 package fundamentos;
 public class Fundamentos {
     public static void main(String[] args) {
-        //Declaração de variaveis
+        //Declaração de variaveis de tipos primitivos
         String nome = "Victor";
         int idade = 16;
         boolean estudando = true;
         double media = 5.5;
         
+        String bairro = new String("Centro");
     }
     
 }
